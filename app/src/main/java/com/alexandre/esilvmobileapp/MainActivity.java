e085c12c5b3e1a8cddbe8f44692e71e765bd8e3d
alexr1221 <alexr2030@gmail.com>
2020-03-26 11:44:17 +0100

    ui(menu) Changed menu items

--- app/src/main/java/com/alexandre/esilvmobileapp/MainActivity.java
+++ app/src/main/java/com/alexandre/esilvmobileapp/MainActivity.java
@@ -21,9 +21,6 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -47,8 +44,8 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_dashboard, R.id.nav_reviews, R.id.nav_ratings,
+                R.id.nav_settings, R.id.nav_switchgame, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
